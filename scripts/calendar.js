44e69ecfe53379319b02c2d9451572bef96ca171
Nick Noel <nicknoel251289@gmail.com>
2017-03-20 00:22:34 -0400

    Stylized the color and text and added in all scrolls

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,44 +1,43 @@
-(function ($) {
-
+    (function ($) {
         function init() {
-
-            mobiscroll.scroller('#test', {
-                theme: theme,
-                display: display,
+            mobiscroll.scroller('#event-date-input', {
+                theme: 'mobiscroll-dark',
+                display: 'bottom',
                 lang: lang,
                 wheels: [
                     [{
                         label: 'First wheel',
-                        data: ['0', '1', '2', '3', '4', '5', '6', '7']
+                        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
                     }, {
                         label: 'Second wheel',
-                        data: [{
-                            value: 0,
-                            display: 'a'
-                        }, {
-                            value: 1,
-                            display: 'b'
-                        }, {
-                            value: 2,
-                            display: 'c'
-                        }, {
-                            value: 3,
-                            display: 'd'
-                        }]
+                        data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+                    }, {
+                        label: 'Third wheel',
+                        data: ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024']
                     }]
                 ]
             });
+            
+            mobiscroll.scroller('#event-time-input', {
+                theme: 'mobiscroll-dark',
+                display: 'bottom',
+                lang: lang,
+                wheels: [
+                    [{
+                        label: 'First wheel',
+                        data: ['12', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11']
+                    }, {
+                        label: 'Second wheel',
+                        data: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '00',]
+                    }, {
+                        label: 'Third wheel',
+                        data: ['AM', 'PM']
+                    }]
+                ]
+            });
+            
         }
-
         var theme, display, lang;
-
-        $('.settings').on('change', function () {
-            theme = $('#theme').val() || mobiscroll.settings.theme;
-            display = $('#display').val();
-            lang = $('#language').val();
-
-            init();
-        });
-
-        $('#theme').trigger('change');
+        init();
     })(mobiscroll.$);
+
