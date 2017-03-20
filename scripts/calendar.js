449494080d04accad1fa8bc63da155a508ae0505
Nick Noel <nicknoel251289@gmail.com>
2017-03-19 21:40:59 -0400

    Added mobi files

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,19 +1,44 @@
-/* 
-$( document ).on( "pagecreate", pageselector, function() {
-    var picker = $( "input[type='text']", this );
-    picker.mobipick();
-});
+(function ($) {
 
-picker.on( "change", function() {
-    // formatted date, like yyyy-mm-dd              
-    var date = $( this ).val();
- 
-    // JavaScript Date object
-    var dateObject = $( this ).mobipick( "option", "date" );
-});
-*/ 
+        function init() {
 
-var working = function (){
-    alert('Javascript works');
-}
-working();
+            mobiscroll.scroller('#test', {
+                theme: theme,
+                display: display,
+                lang: lang,
+                wheels: [
+                    [{
+                        label: 'First wheel',
+                        data: ['0', '1', '2', '3', '4', '5', '6', '7']
+                    }, {
+                        label: 'Second wheel',
+                        data: [{
+                            value: 0,
+                            display: 'a'
+                        }, {
+                            value: 1,
+                            display: 'b'
+                        }, {
+                            value: 2,
+                            display: 'c'
+                        }, {
+                            value: 3,
+                            display: 'd'
+                        }]
+                    }]
+                ]
+            });
+        }
+
+        var theme, display, lang;
+
+        $('.settings').on('change', function () {
+            theme = $('#theme').val() || mobiscroll.settings.theme;
+            display = $('#display').val();
+            lang = $('#language').val();
+
+            init();
+        });
+
+        $('#theme').trigger('change');
+    })(mobiscroll.$);
